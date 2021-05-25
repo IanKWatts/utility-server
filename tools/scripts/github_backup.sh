d75d7da5930cf5553a96ed29088a1e2c5be01ec1
Ian Watts <ian@asdf.com>
2021-05-25 14:58:44 -0700

    Update the right script this time

--- tools/scripts/github_backup.sh
+++ tools/scripts/github_backup.sh
@@ -37,14 +37,10 @@ if [ ! -d $LOGDIR ]; then mkdir -p $LOGDIR; fi
 # Read the repo list and S3 credentials
 # -------------------------------------
 source /etc/github-repos-to-back-up/github-repos-to-back-up.sh
-S3_ID=`cat /etc/github-backups-s3-creds/id`
-S3_URL=`cat /etc/github-backups-s3-creds/URL`
-S3_SECRET=`cat /etc/github-backups-s3-creds/secret`
-REPO_TOKEN=`cat /etc/github-backups-repo-creds/TOKEN`
 
 log() {
   echo "--> $1"
-  #echo "--> $1" >> ${LOGFILE}
+  echo "--> $1" >> ${LOGFILE}
 }
 
 do_backup() {
